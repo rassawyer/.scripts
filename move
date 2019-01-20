be541a39627e64211a927200a8524423262abe40
Robert <rassawyer@gmail.com>
2019-01-19 23:07:02 -0500

    updated package lists

--- move
+++
@@ -1,5 +0,0 @@
-find . -name *.mp4 -exec mv ../ \;
-
-find . -name *.mkv -print -execdir mv '{}' ../ \;
-
-find -type d -empty -delete
--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
-## remove ~/.bashrc and replice it with a softlink to custom version.
+# pull any missing sripts and dotfiles from github
+git clone https://github.com/rassawyer/.scripts.git
+
+# remove ~/.bashrc and replice it with a softlink to custom version.
 rm -rfv ~/.bashrc
 ln -sf ~/.scripts/dotFiles/bashrc ~/.bashrc
 
-## remove existing i3 config file, and replace with a softlink to custom version 
+# remove existing i3 config file, and replace with a softlink to custom version 
 rm -rfv ~/.config/i3/config
 ln -sf ~/.scripts/dotFiles/i3 ~/.config/i3/config
 
-## remove ~/.xinitrc and replace with softlink to custom version
+# remove ~/.xinitrc and replace with softlink to custom version
 rm -rfv ~/.xinitrc
 ln -sf ~/.scripts/dotFiles/xinitrc ~/.xinitrc
 
-## Create .bak directory under /home/$USER to store the backups of pacman db
+# Create .bak directory under /home/$USER to store the backups of pacman db
 mkdir /home/$USER/.bak
