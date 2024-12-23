bdda903b04d5bb7bef2b67da71c85cbbb441db45
Robert <rassawyer@gmail.com>
2024-12-23 14:51:13 -0500

    Update: change setup script for ZSH

--- setup.sh
+++ setup.sh
@@ -1,30 +1,20 @@
-###############################################################################
+################################################################################
 ## install neccesary packages
 pacman -S --needed git
-###############################################################################
+################################################################################
 
-###############################################################################
+################################################################################
 ## git clone repo and all of the resources
 git pull https://github.com/rassawyer/.scripts.git
-###############################################################################
+################################################################################
 
-###############################################################################
-## remove existing .bashrc, if any exists, and softlink to custom version
-if [ ! -f $HOME/.bashrc ]; then
-	rm -rfv $HOME/.bashrc
-fi	
-rm -rfv $HOME/.bashrc
-ln -sf $HOME/.scripts/dotFiles/bashrc $HOME/.bashrc
-###############################################################################
-
-###############################################################################
-## check if neccesary directories exist, create them if they do not, then
-## create softlinks to custom files.
+################################################################################
+## check if neccesary directories exist, create them if they do not
 if [ ! -d $HOME/.config ]; then
 	mkdir $HOME/.config
 fi
-if [ ! -d $HOME/.config/i3 ]; then
-	mkdir $HOME/.config/i3
+if [ ! -d $HOME/.config/sway ]; then
+	mkdir $HOME/.config/sway
 fi
 
 if [ ! -d $HOME/.scripts ]; then
@@ -34,31 +24,27 @@ fi
 if [ ! -d $HOME/.bak ]; then
 	mkdir $HOME/.bak
 fi
-###############################################################################
+################################################################################
 
-###############################################################################
-## remove existing i3 config file, and replace with a softlink to custom 
+################################################################################
+## Create symlinks for the zsh config files.
+ln -sf $HOME/.scripts/dotFiles/zsh/zshenv $HOME/.zshenv
+ln -sf $HOME/.scripts/dotFiles/zsh/zshrc $HOME/.config/zsh.zshrc
+ln -sf $HOME/.scripts/dotFiles/zsh/completion.zsh $HOME/.config/zsh/completions.zsh
+################################################################################
+
+################################################################################
+## remove existing sway config file, and replace with a softlink to custom 
 ## version
-if [ ! -f $HOME/.config/i3/config ]; then
-	rm -rfv $HOME/.config/i3/config
+if [ ! -f $HOME/.config/sway/config ]; then
+	rm -rfv $HOME/.config/sway/config
 fi
-rm -rfv $HOME/.config/i3/config
-ln -sf $HOME/.scripts/dotFiles/i3 $HOME/.config/i3/config
-###############################################################################
+rm -rfv $HOME/.config/sway/config
+ln -sf $HOME/.scripts/dotFiles/sway $HOME/.config/sway/config
+################################################################################
 
-###############################################################################
+################################################################################
 ## setup Android build utilities and directories
 curl https://storage.googleapis.com/git-repo-downloads/repo > $HOME/.scripts/repo
 chmod a+x $HOME/.scripts/repo
-###############################################################################
-
-###############################################################################
-## remove existing i3blocks config file, if present, and replace with softlink 
-## to custom version.
-## remove $HOME/.xinitrc and replace with softlink to custom version
-if [ ! -f $HOME/.xinitrc ]; then
-	rm -rfv $HOME/.xinitrc
-fi
-rm -rfv $HOME/.xinitrc
-ln -sf $HOME/.scripts/dotFiles/xinitrc $HOME/.xinitrc
-###############################################################################
+################################################################################
