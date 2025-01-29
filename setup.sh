8e9eb97e73a237d689939a1842040b19e97b27fa
Robert <rassawyer@gmail.com>
2025-01-29 16:00:02 -0500

    update: mv .config contents out of dotFiles, update setup script, add .gitignore to ignore all the detritus that accumulates in .config

--- setup.sh
+++ setup.sh
@@ -10,41 +10,26 @@ git pull https://github.com/rassawyer/.scripts.git
 
 ################################################################################
 ## check if neccesary directories exist, create them if they do not
-if [ ! -d $HOME/.config ]; then
-	mkdir $HOME/.config
-fi
-if [ ! -d $HOME/.config/sway ]; then
-	mkdir $HOME/.config/sway
-fi
-
-if [ ! -d $HOME/.scripts ]; then
-	mkdir $HOME/.scripts
+if [ -d "$HOME/.config" ]; then
+	echo "Backing up .config"
+	mv "$HOME/.config" "$HOME/.config_$(date +%Y%m%d_%H%M%S).bak"
+	echo "Deleting .config"
+	rm -rf "$HOME/.config"
 fi
 
-if [ ! -d $HOME/.bak ]; then
-	mkdir $HOME/.bak
+if [ ! -L "$HOME/.config" ]; then
+    echo "Creating symlink for ~/.config"
+    ln -s "$HOME/.scripts/dotFiles" "$HOME/.config"
 fi
 ################################################################################
 
 ################################################################################
 ## Create symlinks for the zsh config files.
-ln -sf $HOME/.scripts/dotFiles/zsh/zshenv $HOME/.zshenv
-ln -sf $HOME/.scripts/dotFiles/zsh/zshrc $HOME/.config/zsh.zshrc
-ln -sf $HOME/.scripts/dotFiles/zsh/completion.zsh $HOME/.config/zsh/completions.zsh
-################################################################################
-
-################################################################################
-## remove existing sway config file, and replace with a softlink to custom 
-## version
-if [ ! -f $HOME/.config/sway/config ]; then
-	rm -rfv $HOME/.config/sway/config
-fi
-rm -rfv $HOME/.config/sway/config
-ln -sf $HOME/.scripts/dotFiles/sway $HOME/.config/sway/config
+ln -sf "$HOME/.scripts/dotFiles/zshenv" "$HOME/.zshenv"
 ################################################################################
 
 ################################################################################
 ## setup Android build utilities and directories
 curl https://storage.googleapis.com/git-repo-downloads/repo > $HOME/.scripts/repo
-chmod a+x $HOME/.scripts/repo
+chmod a+x "$HOME/.scripts/repo"
 ################################################################################
